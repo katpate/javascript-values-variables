e3b3dbc0acb3d18d66941289f602c7a28ffa07ca
fn <j.k.13@hotmail.com>
2025-07-05 23:04:57 +0700

    feat(ex1and2): edit firstname and lastname

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,6 +1,6 @@
 //Exercise #1: Variable Creation
-let firstName = "John";
-let lastName = "Doe";
+let firstName = "Karter";
+let lastName = "Anderson";
 let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
 const restaurantName = "Chef's Table by Chef James";
@@ -11,4 +11,4 @@ const numRestaurantOpen = 1500;
 //Exercise #2: Variable Reassignment
 address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
 netFoodPrice = 5000;
-numRestaurantOpen = 2500;
+numRestaurantOpen = 2500; 
